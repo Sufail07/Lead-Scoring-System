e4bc0c65c616a30d2a2ab598c884cab4b7322b10
Sufail Salim <sufailsalim07@gmail.com>
2025-09-16 13:58:37 +0530

    Changed url path naming

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 
 urlpatterns = [
     path('offer/', offer, name='offer'),
-    path('upload_leads/<int:offer_id>/', upload_leads, name='upload_leads'),
+    path('leads/upload/<int:offer_id>/', upload_leads, name='upload_leads'),
     path('score/<int:offer_id>/', get_leads_score, name='get_leads_score'),
     path('result/', result, name='result'),
     path('view_leads/', view_leads, name='view_leads'),
